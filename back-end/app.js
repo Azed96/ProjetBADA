fdc526bd06ef437c19154e988013c7249237ea08
timorhim <brahim.timor@gmail.com>
2020-02-13 21:36:45 +0100

    Connexion MongoDB + CORS origin

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+app.use(bodyParser.json());
+
+// Allow CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+// Connexion to mongoDb
+mongoose.connect('mongodb+srv://root:root@bada-fvexe.mongodb.net/test',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 
 app.use((req, res, next) => {
