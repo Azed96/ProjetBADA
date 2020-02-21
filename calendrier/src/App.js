4956b506f20b9dc57191bacb4740086c9d5c5b75
Badis Meddouri <meddouri.badis@gmail.com>
2020-02-21 09:25:44 +0100

    Fix error

--- calendrier/src/App.js
+++ calendrier/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, MonthAgenda, TimelineViews, TimelineMonth} from '@syncfusion/ej2-react-schedule';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject} from '@syncfusion/ej2-react-schedule';
 
 class App extends React.Component {
 
@@ -9,22 +9,24 @@ class App extends React.Component {
     this.data = [{
       Id: 2,
       Subject: 'Meeting',
-      StartTime: new Date(2018, 1, 15, 10, 0),
-      EndTime: new Date(2018, 1, 15, 12, 30),
+      StartTime: new Date(2020, 1, 15, 10, 0),
+      EndTime: new Date(2020, 1, 15, 12, 30),
       IsAllDay: false,
       Status: 'Completed',
-      Priority: 'High'
+      Priority: 'High',
+      IsReadonly: true
     }];
   }
 
   render() {
-    return <ScheduleComponent height='550px' selectedDate={new Date(2018, 1, 15)} eventSettings={{ dataSource: this.data,
+    return <ScheduleComponent height='550px' isReadOnly={true} selectedDate={new Date(2020, 1, 15)} eventSettings={{ dataSource: this.data,
       fields: {
         id: 'Id',
         subject: { name: 'Subject' },
         isAllDay: { name: 'IsAllDay' },
         startTime: { name: 'StartTime' },
-        endTime: { name: 'EndTime' }
+        endTime: { name: 'EndTime' },
+        isReadOnly: {name: 'IsReadOnly'}
       }
     }}>
       <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
