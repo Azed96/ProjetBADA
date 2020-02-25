3df2af5c1092adb20805d737cbb9aa9b00c65e0c
Azed96 <Azed96>
2020-02-25 11:33:26 +0100

    Login/out Without front

--- back-end/login_out.js
+++ back-end/login_out.js
@@ -8,16 +8,33 @@
             "GROUPES.LES_ETUDIANTS_DU_GROUPE.UN_CODE_ETUDIANT" : req.body.codeEtudiant
         },function (err,data){
             if(err){
-                console.log("ERREUR");
-                return res.status(500).send();
-            }else{
+                res.json({
+                    status: 0,
+                    message: err
+                });
+            }
+                if(!data){
+                    res.json({
+                        status: 0,
+                        msg: "not found"
+                    });
+
+                }
+            
                 console.log("Connected");
                 res.redirect('/seance/');// rajouter le code
-                return res.status(200).send(data);
-                }
+
+                res.json({
+                    status: 1,
+                    id: data._id,
+                    message: " success"
+                });
+                
         });
 
     });
+
+    
 
     app.get('/logout', (req, res) => {
         req.logout();
